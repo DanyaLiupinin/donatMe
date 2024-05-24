6544690e2a6b5b07f39cbec358d2fb86a70d2d2e
Danya <mr.kovan_2002@mail.ru>
2024-05-24 18:47:43 +0300

    feat(auth): auth basic functionality finished

--- src/connector.tsx
+++ src/connector.tsx
@@ -1,5 +1,8 @@
 import TonConnect from "@tonconnect/sdk";
 
-export const connector = new TonConnect({ manifestUrl: "" });
+export const connector = new TonConnect({
+  manifestUrl:
+    "https://raw.githubusercontent.com/DanyaLiupinin/donatMe/master/tonconnect-manifest.json",
+});
 
 connector.restoreConnection();
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,19 +2,23 @@ import { Link } from "react-router-dom";
 import { useWallet } from "../../hooks/useWallet";
 
 export const Menu = () => {
-  const wallet = useWallet();
+  const wallet: any = useWallet();
 
+  console.log("wa");
   console.log(wallet);
 
   return (
     <div>
-      {wallet && <p className="text-white">{wallet}</p>}
-      <Link
-        to="/ton-wallets"
-        className="text-white outline outline-1 outline-white p-2 fixed top-5 right-5"
-      >
-        Authorization
-      </Link>
+      {wallet ? (
+        <p className="text-white">{wallet?.account?.address}</p>
+      ) : (
+        <Link
+          to="/ton-wallets"
+          className="text-white outline outline-1 outline-white p-2 fixed top-5 right-5"
+        >
+          Authorization
+        </Link>
+      )}
       <button className="text-white outline outline-1 outline-white p-2 absolute mx-auto my-auto w-fit h-fit top-0 bottom-0 left-0 right-0">
         Donat
       </button>
