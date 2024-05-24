7c4749c3262f4f937bba4c14628edf13c81d31a7
Danya <mr.kovan_2002@mail.ru>
2024-05-24 16:57:13 +0300

    feat(walletList): back button added to <TonWalletsList />

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const Menu = () => {
   return (
     <div>
-      <button
+      <Link
+        to="/ton-wallets"
         className="text-white outline outline-1 outline-white p-2 fixed top-5 right-5"
-        type="button"
       >
         Authorization
-      </button>
+      </Link>
       <button className="text-white outline outline-1 outline-white p-2 absolute mx-auto my-auto w-fit h-fit top-0 bottom-0 left-0 right-0">
         Donat
       </button>
--- src/pages/TonWalletsList/TonWalletsList.tsx
+++ src/pages/TonWalletsList/TonWalletsList.tsx
@@ -4,6 +4,7 @@ import {
 } from "@tonconnect/ui-react";
 import { connector } from "../../connector";
 import { Section, WalletBadge } from "@components";
+import { Link } from "react-router-dom";
 
 export const TonWalletsList = ({ walletList }: any) => {
   const onClickWallet = (walletInfo: any) => {
@@ -20,6 +21,9 @@ export const TonWalletsList = ({ walletList }: any) => {
 
   return (
     <div className="absolute mx-auto my-auto top-0 left-0 right-0 bottom-0 w-[90%] h-[90%] flex justify-center flex-col p-[10px] gap-3">
+      <Link className="text-white absolute left-5 top-0" to="/">
+        Back
+      </Link>
       <div className="m-auto flex flex-col gap-3">
         {walletList &&
           walletList.map((wallet: any, index: number) => (
