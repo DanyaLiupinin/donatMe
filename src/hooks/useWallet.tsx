eecf366119eb7d0a6eb2e2d06a495674a767397f
Danya <mr.kovan_2002@mail.ru>
2024-05-24 18:07:08 +0300

    feat(hooks): useWallet hook added

---
+++ src/hooks/useWallet.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { connector } from "../connector";
+
+export const useWallet = () => {
+  const [wallet, setWallet] = useState(null);
+
+  useEffect(() => {
+    return connector.onStatusChange((e: any) => {
+      setWallet(e);
+    });
+  }, []);
+
+  return wallet;
+};
