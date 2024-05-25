61e2b579adb129655a43c6770395b7c57eea6c74
Danya <mr.kovan_2002@mail.ru>
2024-05-25 12:48:59 +0300

    feat(spinner): test spinner added

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -18,6 +18,8 @@ export const Menu = () => {
     await connector.disconnect();
   };
 
+  console.log(wallet);
+
   return (
     <div>
       {wallet ? (
--- src/pages/TonWalletsList/TonWalletsList.tsx
+++ src/pages/TonWalletsList/TonWalletsList.tsx
@@ -6,10 +6,17 @@ import {
 } from "@tonconnect/sdk";
 import { QRModal } from "@components";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useWallet } from "../../hooks/useWallet";
+import { useNavigate } from "react-router-dom";
 
 export const TonWalletsList = ({ walletList }: any) => {
   const [selectedWallet, setSelectedWallet] = useState<any>();
+  const [isSpinner, setIsSpinner] = useState(false);
+  const wallet = useWallet();
+  const navigate = useNavigate();
+
+  const onCloseQRModal = () => setSelectedWallet("");
 
   const onClickWallet = (walletInfo: any) => {
     if (isWalletInfoRemote(walletInfo)) {
@@ -23,26 +30,46 @@ export const TonWalletsList = ({ walletList }: any) => {
     window.open(walletInfo.aboutUrl, "_blank");
   };
 
+  useEffect(() => {
+    if (wallet && selectedWallet) {
+      setIsSpinner(true);
+      setTimeout(() => {
+        onCloseQRModal();
+        setIsSpinner(false);
+        navigate("/");
+        window.location.reload();
+      }, 2000);
+    }
+  }, [wallet, selectedWallet]);
+
   return (
-    <div className="h-[100%] mx-auto my-auto top-0 left-0 right-0 bottom-0 w-[90%] h-[90%] flex justify-center flex-col p-[10px] gap-3">
-      <Link className="text-white absolute left-5 top-0" to="/">
-        Back
-      </Link>
-      <div className="m-auto flex flex-col gap-3">
-        <QRModal
-          isOpen={selectedWallet ? true : false}
-          onClose={() => setSelectedWallet("")}
-          walletInfo={selectedWallet}
-        />
-        {walletList &&
-          walletList.map((wallet: any, index: number) => (
-            <WalletBadge
-              key={index}
-              wallet={wallet}
-              onClickWallet={onClickWallet}
-            />
-          ))}
+    <>
+      {isSpinner && (
+        <div className="bg-white w-[100px] h-[100px] absolute top-0 left-0 right-0 bottom-0 my-auto mx-auto z-20">
+          <p>Ya spinner</p>
+        </div>
+      )}
+
+      <div className="h-[100%] mx-auto my-auto top-0 left-0 right-0 bottom-0 w-[90%] h-[90%] flex justify-center flex-col p-[10px] gap-3">
+        <Link className="text-white absolute left-5 top-0" to="/">
+          Back
+        </Link>
+        <div className="m-auto flex flex-col gap-3">
+          <QRModal
+            isOpen={selectedWallet ? true : false}
+            onClose={onCloseQRModal}
+            walletInfo={selectedWallet}
+          />
+          {walletList &&
+            walletList.map((wallet: any, index: number) => (
+              <WalletBadge
+                key={index}
+                wallet={wallet}
+                onClickWallet={onClickWallet}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
