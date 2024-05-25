2073e7915ec80c1d05c8b6acd8c409eb5b15ff7d
Danya <mr.kovan_2002@mail.ru>
2024-05-25 13:27:27 +0300

    feat(WalletAddress): close wallet dropdown on mousedown added

--- src/features/WalletAddress/WalletAddress.tsx
+++ src/features/WalletAddress/WalletAddress.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { connector } from "../../connector";
 
 export const WalletAddress = ({
@@ -8,22 +8,39 @@ export const WalletAddress = ({
   address: string;
   className?: string;
 }) => {
-  const [isSelectOpened, setSelectOpened] = useState(false);
+  const [isDropdownOpened, setDropdownOpened] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const onDisconnectWallet = async () => {
     await connector.disconnect();
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setDropdownOpened(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={className}>
       <p
-        onClick={() => setSelectOpened(!isSelectOpened)}
+        onClick={() => setDropdownOpened(!isDropdownOpened)}
         className="text-white text-end"
       >
         {address}
       </p>
-      {isSelectOpened && (
-        <div className="bg-white rounded">
+      {isDropdownOpened && (
+        <div ref={dropdownRef} className="bg-white rounded">
           <button onClick={onDisconnectWallet}>Disconnect</button>
         </div>
       )}
